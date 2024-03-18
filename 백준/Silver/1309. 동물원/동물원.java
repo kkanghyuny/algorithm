94ce92ac913f0b42b7bc977cfbffebc5397a5032
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-03-18 16:11:54 +0900

    [Silver I] Title: 동물원, Time: 116 ms, Memory: 13220 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+	
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 3;
+		
+		
+		for(int i = 2; i < n + 1; i++) {
+			dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}

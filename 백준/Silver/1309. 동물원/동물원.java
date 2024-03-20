038317a300fc4e2139666d64a7967cc05a9f7020
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-03-20 14:20:05 +0900

    [Silver I] Title: 동물원, Time: 88 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,11 +1,12 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 	
 		int[] dp = new int[n + 1];
 		dp[0] = 1;

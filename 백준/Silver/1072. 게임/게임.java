81aebe370d2890224d4700d164cb09faefcdb256
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-03-29 15:59:39 +0900

    [Silver III] Title: 게임, Time: 76 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		long x = sc.nextInt();
-		long y = sc.nextInt();		
+		int x = Integer.parseInt(st.nextToken());
+		long y = Integer.parseInt(st.nextToken());
 		long z = ((long)y * 100 / x);
 		
-		int game = (int)Math.ceil((100 * y - x * (z + 1)) / (double)(z - 99));
+		int game = (int)Math.ceil((double)(100 * y - x * (z + 1)) / (z - 99));
 
 		if (z == 100 || z == 99) {
 			System.out.println(-1);

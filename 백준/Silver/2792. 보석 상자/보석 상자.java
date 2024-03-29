0d16c671c4dab3152ef237e7241590214b192ee0
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-03-29 15:42:34 +0900

    [Silver I] Title: 보석 상자, Time: 308 ms, Memory: 34796 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -11,22 +11,23 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int[] arr = new int[m];
-		long left = 1;
-		long right = 0;
+		int left = 1;
+		int right = 0;
 
 		for (int i = 0; i < m; i++) {
 			int cnt = Integer.parseInt(br.readLine());
 			arr[i] = cnt;
-			right += cnt;
+			right = Math.max(right, cnt);
 		}
 		
+		right++;
+		
 		while(left < right) {
-			long mid = (left + right) / 2;
+			int mid = (left + right) / 2;
 			int cnt = 0;
 			for(int i = 0; i < m; i++) {
 				cnt += ((arr[i] - 1) / mid) + 1; 
 			}
-			
 			if(cnt == n) {
 				right = mid;
 			} else if(cnt > n) {

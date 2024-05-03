22679f4a1e90ae588e2c5b51847b1270febc6a2e
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-05-03 16:22:45 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 560 ms, Memory: 50668 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -5,11 +5,28 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static class Edge implements Comparable<Edge>{
+		int u;
+		int v;
+		int w;
+		
+		public Edge(int u, int v, int w) {
+			this.u = u;
+			this.v = v;
+			this.w = w;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return this.w - o.w;
+		}
+	}
+	
 	static int[] arr;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
 		
 		int v = Integer.parseInt(st.nextToken());
 		int e = Integer.parseInt(st.nextToken());
@@ -22,21 +39,21 @@ public class Main {
 		
 		for(int i = 0; i < e; i++) {
 			st = new StringTokenizer(br.readLine());
-			pq.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+			pq.offer(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 		
 		int pick = 0;
 		int answer = 0;
 		
 		while(!pq.isEmpty() && pick < (v - 1)) {
-			int[] poll = pq.poll();
-			int parentX = find(poll[0]);
-			int parentY = find(poll[1]);
+			Edge poll = pq.poll();
+			int parentX = find(poll.u);
+			int parentY = find(poll.v);
 			
 			if(parentX != parentY) {
 				union(parentX, parentY);
 				pick++;
-				answer += poll[2];
+				answer += poll.w;
 			}
 		}
 		
@@ -45,7 +62,7 @@ public class Main {
 	
 	static int find(int x) {
 		if(x == arr[x]) return x;
-		return find(arr[x]);
+		return arr[x] = find(arr[x]);
 	}
 	
 	static void union(int x, int y) {

74092050b9cac8ff2e9674a93187738d3099d709
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-05-11 13:59:41 +0900

    [Gold IV] Title: 플로이드, Time: 368 ms, Memory: 43968 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int inf = 987654321;
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i != j) arr[i][j] = inf;
+			}
+		}
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			int c = Integer.parseInt(st.nextToken());
+			
+			arr[a][b] = Math.min(c, arr[a][b]);
+		}
+
+		for(int k = 0; k < n; k++) {
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+				}
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(arr[i][j] == inf) {
+					sb.append(0).append(" ");
+				} else {
+					sb.append(arr[i][j]).append(" ");
+				}
+			}
+			sb.append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}

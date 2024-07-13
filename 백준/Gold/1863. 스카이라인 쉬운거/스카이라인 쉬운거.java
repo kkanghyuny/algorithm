ee12958322daf523f64633353c11c4a635979bf3
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-07-13 12:30:39 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 136 ms, Memory: 17184 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -6,51 +6,29 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static class Point implements Comparable<Point> {
-		int x;
-		int y;
-
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public int compareTo(Point o) {
-			return this.x - o.x;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Point> pq = new PriorityQueue<>();
 		Stack<Integer> stack = new Stack<>();
 		int answer = 0;
 
 		while(n-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
+			st.nextToken();
 			int y = Integer.parseInt(st.nextToken());
-			pq.add(new Point(x, y));
-		}
 
-		while(!pq.isEmpty()) {
-			Point curr = pq.poll();
-			int high = curr.y;
 
-			while(!stack.isEmpty() && stack.peek() > high) {
+			while(!stack.isEmpty() && stack.peek() > y) {
 				stack.pop();
 				answer++;
 			}
 
-			if(!stack.isEmpty() && stack.peek() == high) continue;
-			if(high == 0) continue;
+			if(!stack.isEmpty() && stack.peek() == y) continue;
+			if(y == 0) continue;
 
-			stack.push(high);
+			stack.push(y);
 		}
 
 		answer += stack.size();

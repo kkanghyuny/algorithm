d7090b44cab6c871ff00887bcc94fed2ac4a6e49
kkanghyuny <130138811+kkanghyuny@users.noreply.github.com>
2024-08-04 22:59:17 +0900

    [level 2] Title: 올바른 괄호, Time: 21.28 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                stack.add(c);
+            } else {
+                if(!stack.isEmpty() && stack.peek() == '(') stack.pop();
+                else answer = false;
+            }
+        }
+        
+        if(!stack.isEmpty()) answer = false; 
+
+        return answer;
+    }
+}

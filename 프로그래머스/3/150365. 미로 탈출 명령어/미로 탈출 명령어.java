65bc34f01bf175f70d4726f92859aebef9b71054
Kanghyun Lee <130138811+kkanghyuny@users.noreply.github.com>
2024-10-10 12:22:46 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 140.10 ms, Memory: 213 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    
+    static int row, col, endR, endC, endDepth;
+    static int[] dr = {1, 0, 0, -1};
+    static int[] dc = {0, -1, 1, 0};
+    static boolean isEnd;
+    static boolean[][][] vis;
+    static String answer;
+    static String[] cmd = {"d", "l", "r", "u"};
+    
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        if(Math.abs(Math.abs(x - r) + Math.abs(y - c) - k) % 2 != 0) return "impossible";
+        row = n;
+        col = m;
+        endR = r - 1;
+        endC = c - 1;
+        endDepth = k;
+        isEnd = false;
+        answer ="impossible";
+        vis = new boolean[n][m][k + 1];
+        dfs(x - 1, y - 1, 0, "");
+        return answer;
+    }
+    
+    static void dfs(int nowR, int nowC, int depth, String s) {
+        if(isEnd) return;
+        
+        if(depth == endDepth && nowR == endR && nowC == endC) {
+            answer = s;
+            isEnd = true;
+            return;
+        } else if(depth == endDepth) {
+            return;
+        }
+        
+        vis[nowR][nowC][depth] = true;
+        
+        for(int d = 0; d < 4; d++) {
+            int nr = nowR + dr[d];
+            int nc = nowC + dc[d];
+            
+            if(!checkBoundary(nr, nc)) continue;
+            if(vis[nr][nc][depth + 1]) continue;
+            dfs(nr, nc, depth + 1, s + cmd[d]);
+        }
+    }
+    
+    static boolean checkBoundary(int r, int c) {
+        return r >= 0 && r < row && c >= 0 && c < col;
+    }
+}

dd172bf10aa2df6479dfbcd1942b06a065f52ce9
Kanghyun Lee <130138811+kkanghyuny@users.noreply.github.com>
2024-12-24 11:52:09 +0900

    [Gold II] Title: 대표 선수, Time: 944 ms, Memory: 167852 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -2,43 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[n][m];
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < m; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for(int i = 0; i < n; i++) {
-			Arrays.sort(arr[i]);
-		}
-		
-		int answer = Integer.MAX_VALUE;
-		int[] idxArr = new int[n];
-		int cnt = 0;
-		while(true) {
-			int max = Integer.MIN_VALUE;
-			int min = Integer.MAX_VALUE;
-			int semiMin = Integer.MAX_VALUE;
-			int minIdx = 0;
-			for(int i = 0; i < n; i++) {
-				if(arr[i][idxArr[i]] > max) max = arr[i][idxArr[i]];
-				if(arr[i][idxArr[i]] < min) {
-					min = arr[i][idxArr[i]];
-					minIdx = i;
-				}
-			}
-			answer = Math.min(max - min, answer);
-			idxArr[minIdx]++;
-			if(idxArr[minIdx] >= m) break;
-		}
-		System.out.println(answer);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a ,b) -> a[0] - b[0]);
+        
+        int[][] arr = new int[n][m];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr[i]);
+            pq.offer(new int[]{arr[i][0], i, 0});
+            max = Math.max(max, arr[i][0]);
+        }
+
+        int answer = Integer.MAX_VALUE;
+        while (true) {
+            int[] curr = pq.poll();
+            int min = curr[0];
+            int groupIdx = curr[1];
+            int elementIdx = curr[2];
+            answer = Math.min(answer, max - min);
+
+            if (elementIdx + 1 == m) break;
+
+            int nextValue = arr[groupIdx][elementIdx + 1];
+            pq.offer(new int[]{nextValue, groupIdx, elementIdx + 1});
+            max = Math.max(max, nextValue);
+        }
+        System.out.println(answer);
+    }
 }
